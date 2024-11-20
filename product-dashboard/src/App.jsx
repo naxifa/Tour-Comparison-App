310220270f16dd7b5492cd648a4f15312055692a
naxifa <maliha1@usf.edu>
2024-11-19 20:48:01 -0500

    Setup root component with global state and routing.

---
+++ product-dashboard/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Gallery from "./components/Gallery";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tour Comparison App</h1>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
